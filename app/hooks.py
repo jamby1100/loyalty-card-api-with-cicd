b77c67e7f3fc87bb659b2622dd57d5446ef91e76
Raphael Jambalos <rajambalos@Raphaels-MacBook-Pro.local>
2021-10-31 19:07:34 +0800

    Add codedeploy hook responses

--- app/hooks.py
+++ app/hooks.py
@@ -1,7 +1,34 @@
+import boto3
+
 def pre(event, context):
     print("PRE HOOK")
     print(event)
 
+    lifecycleEventHookExecutionId = event['LifecycleEventHookExecutionId']
+    deploymentId = event['DeploymentId']
+    deployment_status = 'Succeeded'
+
+    client = boto3.client('codedeploy')
+
+    response = client.put_lifecycle_event_hook_execution_status(
+        deploymentId=deploymentId,
+        lifecycleEventHookExecutionId=lifecycleEventHookExecutionId,
+        status=deployment_status
+    )
+
+
 def post(event, context):
     print("POST HOOK")
     print(event)
+
+    lifecycleEventHookExecutionId = event['LifecycleEventHookExecutionId']
+    deploymentId = event['DeploymentId']
+    deployment_status = 'Succeeded'
+
+    client = boto3.client('codedeploy')
+
+    response = client.put_lifecycle_event_hook_execution_status(
+        deploymentId=deploymentId,
+        lifecycleEventHookExecutionId=lifecycleEventHookExecutionId,
+        status=deployment_status
+    )
